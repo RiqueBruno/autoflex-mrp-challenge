34dc5524f56d805c336e2517571427c12c03b0a9
Bruno <riquebruno@yahoo.com.br>
2026-02-28 20:50:38 -0300

    feat(api): Initial setup of the Spring Boot project.

---
+++ backend/src/main/java/com/riquebruno/autoflex_mrp_api/AutoflexMrpApiApplication.java
@@ -0,0 +1,13 @@
+package com.riquebruno.autoflex_mrp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoflexMrpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoflexMrpApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/riquebruno/autoflex_mrp_api/AutoflexMrpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riquebruno.autoflex_mrp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoflexMrpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
