840cdbfa35715e4222a86018230ed2db073bd791
Bruno <riquebruno@yahoo.com.br>
2026-02-28 21:55:01 -0300

    feat(api): add RawMaterial entity

---
+++ backend/src/main/java/com/riquebruno/autoflex_mrp_api/entity/RawMaterial.java
@@ -0,0 +1,35 @@
+package com.riquebruno.autoflex_mrp_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "raw_material")
+public class RawMaterial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "amount")
+    private  Integer amount;
+
+    @Column(updatable = false, name = "created_at")
+    @CreationTimestamp
+    private OffsetDateTime createdAt;
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private OffsetDateTime updatedAt;
+}
