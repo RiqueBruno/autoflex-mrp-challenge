727e4853e766108f5a8e0486f9705178c94c780c
Bruno <riquebruno@yahoo.com.br>
2026-03-11 12:59:16 -0300

    feat(web): add ProductRecipeModal component for displaying product details and recipe

---
+++ frontend/src/components/views/ProductRecipeModal.tsx
@@ -0,0 +1,29 @@
+export const ProductRecipeModal = () => {
+  return (
+    <section>
+      <h3>Product Details</h3>
+      <div>
+        <div>
+          <span>Image</span>
+        </div>
+        <div>
+          <p>
+            <strong>Name:</strong> Product Name
+          </p>
+          <p>
+            <strong>Price:</strong> $19.99
+          </p>
+        </div>
+      </div>
+      <div>
+        <h4>Recipe</h4>
+        <div>
+          <button>Add Recipe Item</button>
+        </div>
+        <div>
+          <p>Recipe table</p>
+        </div>
+      </div>
+    </section>
+  );
+};
